a409e983979c56e849e2fcfa2dc897981382f5fc
Ruben Blanco <rubenblancomanzano@gmail.com>
2023-01-04 23:10:56 +0100

    add 14. Longest Common Prefix

--- easy/14_longest_common_prefix.py
+++ easy/14_longest_common_prefix.py
@@ -1,4 +1,19 @@
 # https://leetcode.com/problems/longest-common-prefix/
 
+
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(strs: list[str]) -> str:
+
+        res = ''
+
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+
+        return res
+
+    strs = ["flower", "flow", "flight"]
+
+    print(longestCommonPrefix(strs))
