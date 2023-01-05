ae257ae463f10afa8ff7a219183ed0905a029b1f
Ruben Blanco <rubenblancomanzano@gmail.com>
2023-01-05 23:42:52 +0100

    update exercise 217

--- easy/217_contains_duplicate.py
+++ easy/217_contains_duplicate.py
@@ -1,9 +1,13 @@
 # https://leetcode.com/problems/contains-duplicate/
 
 class Solution:
-    def containsDuplicate(nums: list[int]) -> bool:
+    def containsDuplicate(self, nums: list[int]) -> bool:
 
         if len(set(nums)) != len(nums):
             return True
 
         return False
+
+
+solution = Solution()
+print(solution.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
