fb281de6a6ff68007662f602d7df1d8983e4efc7
Ruben Blanco <rubenblancomanzano@gmail.com>
2023-01-06 22:48:43 +0100

    add 20. Valid Parentheses

--- easy/20_validad_parentheses.py
+++ easy/20_validad_parentheses.py
@@ -2,3 +2,18 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
+
+        brackets = {'(': ')', '{': '}', '[': ']'}
+        stack = []
+
+        for c in s:
+            if c in brackets:
+                stack.append(c)
+            elif not stack or brackets[stack.pop()] != c:
+                return False
+
+        return True
+
+
+solution = Solution()
+print(solution.isValid(")"))
