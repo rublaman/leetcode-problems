5ad8fd03fcf49e3020e5c9c7647c95fccbee8de6
Ruben Blanco <rubenblancomanzano@gmail.com>
2023-01-08 22:23:35 +0100

    add 242. Valid Anagram

--- easy/242_valid_anagram.py
+++ easy/242_valid_anagram.py
@@ -2,3 +2,12 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
+
+
+solution = Solution()
+print(solution.isAnagram("anagram", "nagadram"))
