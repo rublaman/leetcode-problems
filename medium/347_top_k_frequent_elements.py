237513404b6d72e8e3e9f39dc8ec5a361b5da67c
Ruben Blanco <rubenblancomanzano@gmail.com>
2023-01-23 23:42:21 +0100

    update exercise 347

--- medium/347_top_k_frequent_elements.py
+++ medium/347_top_k_frequent_elements.py
@@ -1,4 +1,13 @@
 # https://leetcode.com/problems/top-k-frequent-elements/
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+
+        res = []
+
+        for i in nums:
+            print(i)
+
+
+solution = Solution()
+solution.topKFrequent([1, 1, 1, 2, 2, 3], 1)
