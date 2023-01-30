d712ca0d172df58c2e30a4a0bb50ae3206bd6085
Ruben Blanco <rubenblancomanzano@gmail.com>
2023-01-30 23:27:18 +0100

    complete exercise 347

--- medium/347_top_k_frequent_elements.py
+++ medium/347_top_k_frequent_elements.py
@@ -3,15 +3,20 @@
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
 
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
         res = []
-
-        if k == 1:
-            for i in nums:
-                aux = i
-
-        else:
-            for i in nums:
-                print(i)
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
 
 
 solution = Solution()
