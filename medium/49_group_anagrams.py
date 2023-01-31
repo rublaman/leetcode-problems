c02964b70a644d93bf0c67e3928a277c836cfa6f
Ruben Blanco <rubenblancomanzano@gmail.com>
2023-01-31 22:59:53 +0100

    complete exercise 49

--- medium/49_group_anagrams.py
+++ medium/49_group_anagrams.py
@@ -1,10 +1,22 @@
 # https://leetcode.com/problems/group-anagrams/
 
+from collections import defaultdict
+
+
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
 
-        for i in strs:
-            print(i)
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)
+
+        return res.values()
 
 
 solution = Solution()
